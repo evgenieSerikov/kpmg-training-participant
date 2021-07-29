78699f7c54c769a1fa843b2325c217c6ef05a82f
Evgenii S <serikoveugenie@gmail.com>
2021-07-29 13:28:55 +0300

    finish - training-two

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+const DefaultCollection = {name: 'Alex', age: '15'}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kpmg-training-participant';
+
+  text: string = 'My super text';
+  // --------------------------------
+  DefaultCollection = DefaultCollection;
+  collection = Object.assign({}, DefaultCollection);
+
+  resetCollection() {
+    this.collection = Object.assign({}, DefaultCollection);
+  }
+  // --------------------------------
+  names = ['Yan', 'Yura', 'Tanya'];
+  // --------------------------------
+  isIfVisible: boolean;
 }
